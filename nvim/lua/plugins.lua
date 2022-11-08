eef5d8b5af1fa065cf75e3a42cdcb4d5c7ec4e61
John Garrett <jacgarrett18@gmail.com>
2022-11-08 10:46:14 -0500

    bring in nvim config from thinkpad

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,7 +2,8 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim' -- Packer can manage itself
-    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-    use 'ellisonleao/gruvbox.nvim' -- theme
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use { "ellisonleao/gruvbox.nvim" }
 end)
