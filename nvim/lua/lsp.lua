4d592b172a9731319ec26330ea17a3d713daa2fc
John Garrett <jacgarrett18@gmail.com>
2023-02-07 13:51:47 -0500

    bring in neovim from macOS

---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,20 @@
+local lspconfig = require 'lspconfig'
+local coq = require 'coq'
+
+lspconfig.ccls.setup {
+  init_options = {
+    compilationDatabaseDirectory = "build";
+    index = {
+      threads = 0;
+    };
+    clang = {
+      excludeArgs = { "-frounding-math"} ;
+    };
+  }
+}
+
+lspconfig.svls.setup {}
+
+lspconfig.ccls.setup(coq.lsp_ensure_capabilities())
+
+require'lspconfig'.tsserver.setup {}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,6 +4,30 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
+  -- colorscheme
   use { "ellisonleao/gruvbox.nvim" }
+  -- neovim for firefox
+  use {
+    'glacambre/firenvim',
+    run = function() vim.fn['firenvim#install'](0) end 
+  }
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  -- completion
+  -- use { 'ms-jpq/coq_nvim', branch = 'coq' }
+  use { 'ms-jpq/coq_nvim', commit = '5eddd31bf8a98d1b893b0101047d0bb31ed20c49'} -- https://github.com/ms-jpq/coq_nvim/issues/531
+  use { 'ms-jpq/coq.artifacts', branch = 'artifacts'}
+  -- java LSP
+  use 'mfussenegger/nvim-jdtls'
+  -- latex TODO
+  -- use 'lervag/vimtex'
+  
+  -- treesitter for syntax highlighting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+    end,
+  }
 end)
